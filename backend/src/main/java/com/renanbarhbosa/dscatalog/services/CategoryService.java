8f88a186c2805937fa691a016e92133914a32128
renanbarhbosa <renan-dealmeida@hotmail.com>
2022-09-03 16:16:01 -0300

    Exception handler

--- backend/src/main/java/com/renanbarhbosa/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/renanbarhbosa/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.renanbarhbosa.dscatalog.services;
 import com.renanbarhbosa.dscatalog.dto.CategoryDTO;
 import com.renanbarhbosa.dscatalog.entities.Category;
 import com.renanbarhbosa.dscatalog.repositories.CategoryRepository;
+import com.renanbarhbosa.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +27,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.get();
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
         return new CategoryDTO(entity);
     }
 }
