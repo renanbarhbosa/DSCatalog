b3f060f273334455c283126a0a098049bdc9a5a4
renanbarhbosa <renan-dealmeida@hotmail.com>
2022-09-03 14:18:55 -0300

    Find by id

--- backend/src/main/java/com/renanbarhbosa/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/renanbarhbosa/dscatalog/resources/CategoryResource.java
@@ -1,11 +1,11 @@
 package com.renanbarhbosa.dscatalog.resources;
 
 import com.renanbarhbosa.dscatalog.dto.CategoryDTO;
-import com.renanbarhbosa.dscatalog.entities.Category;
 import com.renanbarhbosa.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +23,11 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDTO>> findAll() {
         List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
