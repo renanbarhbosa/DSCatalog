2111a6be3a78d9af9387a8e0326cd7dec32b0cb1
renanbarhbosa <renan-dealmeida@hotmail.com>
2022-09-05 18:20:27 -0300

    Delete category, Database exception

--- backend/src/main/java/com/renanbarhbosa/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/renanbarhbosa/dscatalog/services/CategoryService.java
@@ -3,8 +3,11 @@ package com.renanbarhbosa.dscatalog.services;
 import com.renanbarhbosa.dscatalog.dto.CategoryDTO;
 import com.renanbarhbosa.dscatalog.entities.Category;
 import com.renanbarhbosa.dscatalog.repositories.CategoryRepository;
+import com.renanbarhbosa.dscatalog.services.exceptions.DatabaseException;
 import com.renanbarhbosa.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -47,9 +50,18 @@ public class CategoryService {
             entity.setName(dto.getName());
             entity = repository.save(entity);
             return new CategoryDTO(entity);
-        }
-        catch (EntityNotFoundException e) {
+        } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found. " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found. " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity vaiolation.");
         }
     }
 }
